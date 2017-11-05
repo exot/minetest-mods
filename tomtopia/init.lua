e8606550d5a3cb84f5ef4f4e4a9cc56db984b97b
Daniel Borchmann <daniel@algebra20.de>
2017-11-05 14:55:06 +0100

    Add crafting recipe for chalk

--- tomtopia/init.lua
+++ tomtopia/init.lua
@@ -1,37 +1,43 @@
 print("[TomTopia] Loading specific definitions for TomTopia …")
 
 minetest.register_craft({
-      output = 'default:gravel 1',
-      recipe = {{'default:cobble'}},
+  output = 'default:gravel 1',
+  recipe = {{'default:cobble'}},
 })
 
 minetest.register_craft({
-      output = 'default:sand 1',
-      recipe = {{'default:gravel'}},
+  output = 'default:sand 1',
+  recipe = {{'default:gravel'}},
 })
 
 minetest.register_craft({
-      output = 'default:dirt 1',
-      recipe = {{'default:sand'}},
+  output = 'default:dirt 1',
+  recipe = {{'default:sand'}},
 })
 
 minetest.register_craft({
-      output = 'default:desert_sand 1',
-      recipe = {{'default:dirt'}},
+  output = 'default:desert_sand 1',
+  recipe = {{'default:dirt'}},
 })
 
 minetest.register_craft({
-      output = 'default:sand 1',
-      recipe = {{'default:desert_sand'}},
+  output = 'default:sand 1',
+  recipe = {{'default:desert_sand'}},
 })
 
 minetest.register_craft({
-      type = "shapeless",
-      output = "default:clay 4",
-      recipe = {"default:dirt", "default:dirt", "default:sand", "default:sand", "bucket:bucket_water"},
-      replacements = {
-         {"bucket:bucket_water", "bucket:bucket_empty"}
-      }
+  type = "shapeless",
+  output = "default:clay 4",
+  recipe = {"default:dirt", "default:dirt", "default:sand", "default:sand", "bucket:bucket_water"},
+  replacements = {
+     {"bucket:bucket_water", "bucket:bucket_empty"}
+  }
+})
+
+minetest.register_craft({
+  type = "shapeless",
+  output = "darkage:chalk 1",
+  recipe = {"darkage:chalk_powder", "darkage:chalk_powder"}
 })
 
 print("[TomTopia] Loading specific definitions for TomTopia … done.")
